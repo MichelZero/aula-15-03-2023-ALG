268ba3461ecdd5063f05810f0ba8d35a62fa05de
Michel <106674127+MichelZero@users.noreply.github.com>
2023-03-15 09:47:32 -0300

    up

--- ex02.py
+++ ex02.py
@@ -10,12 +10,19 @@ import  requests as rs
 # apiBrasil
 # https://brasilapi.com.br/api/cep/v1/{cep}
 # entrada: cep 
-cep = int(input("informe o CEP: ")) # 58900000
+cep = int(input("informe o CEP: ")) # 58400002 CEP de Campina Grande
 consulta = f"https://brasilapi.com.br/api/cep/v2/{cep}" # f-string 
-print(consulta) # saída da consulta https://brasilapi.com.br/api/cep/v2/58900000
+print(consulta) # saída da consulta https://brasilapi.com.br/api/cep/v2/58400002
 
 requisicao = rs.get(consulta) # fazendo a requisição para o servidor da BrasilAPI e armazenando o resultado na variável requisição
 print(requisicao) # teste 200 ou 404
 cep1 = requisicao.json() # transformando o resultado da requisição em um dicionário
 print(cep1) # printando o dicionário
 
+# extração de dados do dicionário
+print('---- endereço ----')
+print(f"rua: {cep1['street']}")
+print(f"cidade: {cep1['city']}")
+print(f"UF: {cep1['state']}")
+print(f"CEP: {cep1['cep']}")
+print(f"GPS: Long {cep1['location']['coordinates']['longitude']}, Lat {cep1['location']['coordinates']['latitude']}")
