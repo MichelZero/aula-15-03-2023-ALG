66636560bf9544bab7bf55ce9861b57bd90ab206
Michel <106674127+MichelZero@users.noreply.github.com>
2023-03-15 09:43:37 -0300

    up

--- ex02.py
+++ ex02.py
@@ -14,3 +14,8 @@ cep = int(input("informe o CEP: ")) # 58900000
 consulta = f"https://brasilapi.com.br/api/cep/v2/{cep}" # f-string 
 print(consulta) # saída da consulta https://brasilapi.com.br/api/cep/v2/58900000
 
+requisicao = rs.get(consulta) # fazendo a requisição para o servidor da BrasilAPI e armazenando o resultado na variável requisição
+print(requisicao) # teste 200 ou 404
+cep1 = requisicao.json() # transformando o resultado da requisição em um dicionário
+print(cep1) # printando o dicionário
+
