917d96580a38a1dbd9c68da96a7150ce11f848c3
Michel <106674127+MichelZero@users.noreply.github.com>
2023-03-15 09:41:52 -0300

    up

--- ex02.py
+++ ex02.py
@@ -6,3 +6,11 @@
 #
 # importando o modulo requests para fazer requisições HTTP para o servidor 
 import  requests as rs
+
+# apiBrasil
+# https://brasilapi.com.br/api/cep/v1/{cep}
+# entrada: cep 
+cep = int(input("informe o CEP: ")) # 58900000
+consulta = f"https://brasilapi.com.br/api/cep/v2/{cep}" # f-string 
+print(consulta) # saída da consulta https://brasilapi.com.br/api/cep/v2/58900000
+
